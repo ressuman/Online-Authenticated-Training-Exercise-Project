0a4713fcf0055040b16502714a8d5c2184ac7e88
Richard Essuman <ressuman001@gmail.com>
2024-08-31 20:45:06 +0000

    "Added 'use client' directive, imported useFormState, and updated AuthForm component to use useFormState with signUp action, displaying form errors if present."

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,8 +1,14 @@
+"use client";
+
+import { signUp } from "@/actions/auth/auth-actions";
 import Link from "next/link";
+import { useFormState } from "react-dom";
 
 export default function AuthForm() {
+  const [formState, formAction] = useFormState(signUp, {});
+
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -14,6 +20,13 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <button type="submit">Create Account</button>
       </p>
